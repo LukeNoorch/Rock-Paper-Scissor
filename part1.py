e992653a5d3ef9f89ac3c3ae4aac8a54fce065e4
Luke <lukey@dynamic-oit-visitornet101-10-25-26-147.princeton.org>
2023-08-07 14:04:43 -0400

    ok

--- part1.py
+++ part1.py
@@ -1,14 +1,12 @@
 class Player:
-    def __init__(self, player1, player2, balance, choice, game_w, game_l):
+    def __init__(self, player1, player2, choice, game_w, game_l):
         self.player1 = player1
         self.player2 = player2
-        self.balance = balance
         self.choice = choice 
         self.game_w = game_w
         self.game_l = game_l
 
-# wtf is this
+    def Luke(self, balance):
 
 
-
-p1 = Player ("Joe", "Luke", )
+        p1 = Player ("Joe", "Luke",)
